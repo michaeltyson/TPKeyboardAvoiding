e427d190e23683583d30902e966b9c9aebdcbb0a
Michael Tyson <michael@atastypixel.com>
2014-01-10 08:47:28 +1100

    Fix dumb logic problem with prior commit

--- TPKeyboardAvoiding/UIScrollView+TPKeyboardAvoidingAdditions.m
+++ TPKeyboardAvoiding/UIScrollView+TPKeyboardAvoidingAdditions.m
@@ -60,7 +60,7 @@ static const int kStateKey;
     if ( [self isKindOfClass:[TPKeyboardAvoidingScrollView class]] ) {
         state.priorContentSize = self.contentSize;
         
-        if ( [self respondsToSelector:@selector(constraints)] && self.constraints.count == 0 && CGSizeEqualToSize(self.contentSize, CGSizeZero) ) {
+        if ( ![self respondsToSelector:@selector(constraints)] || (self.constraints.count == 0 && CGSizeEqualToSize(self.contentSize, CGSizeZero)) ) {
             // Set the content size, if it's not set. Do not set content size explicitly if auto-layout
             // is being used to manage subviews
             self.contentSize = [self TPKeyboardAvoiding_calculatedContentSizeFromSubviewFrames];
