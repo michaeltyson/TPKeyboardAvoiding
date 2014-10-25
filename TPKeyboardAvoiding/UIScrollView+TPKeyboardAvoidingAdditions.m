4a3aada6f7a27521dcfbcb9bc46109fda8efb520
Gabor Kovacs <gabor.kovacs@mobile-client.com>
2014-10-25 19:52:59 +0200

    Layouting in animation block during responder change causes weird animations inside the text fields, so it is an another workaround to solve the stuttering problem

--- TPKeyboardAvoiding/UIScrollView+TPKeyboardAvoidingAdditions.m
+++ TPKeyboardAvoiding/UIScrollView+TPKeyboardAvoidingAdditions.m
@@ -154,12 +154,11 @@ static const int kStateKey;
     CGPoint idealOffset = CGPointMake(0, [self TPKeyboardAvoiding_idealOffsetForView:[self TPKeyboardAvoiding_findFirstResponderBeneathView:self]
                                                                withViewingAreaHeight:visibleSpace]);
 
-    // Ordinarily we'd use -setContentOffset:animated:YES here, but it does not appear to
-    // scroll to the desired content offset. So we wrap in our own animation block.
-    [UIView animateWithDuration:0.25 animations:^{
-        [self setContentOffset:idealOffset animated:NO];
-        [self layoutIfNeeded];
-    }];
+    // Ordinarily we'd use -setContentOffset:animated:YES here, but it interferes with UIScrollView
+    // behavior which automatically ensures that the first responder is within its bounds
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self setContentOffset:idealOffset animated:YES];
+    });
 }
 
 #pragma mark - Helpers
