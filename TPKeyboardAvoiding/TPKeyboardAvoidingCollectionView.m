01090ae54795bde1e1a30c382a10890728430f6c
Justin Middleton <justin@whistle.com>
2015-02-02 10:46:07 -0800

    Use UIKeyboardWillChangeFrameNotification rather than UIKeyboardWillHideNotification, so that the suggestions frame is handled.

--- TPKeyboardAvoiding/TPKeyboardAvoidingCollectionView.m
+++ TPKeyboardAvoiding/TPKeyboardAvoidingCollectionView.m
@@ -15,7 +15,7 @@
 #pragma mark - Setup/Teardown
 
 - (void)setup {
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(TPKeyboardAvoiding_keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(TPKeyboardAvoiding_keyboardWillShow:) name:UIKeyboardWillChangeFrameNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(TPKeyboardAvoiding_keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(scrollToActiveTextField) name:UITextViewTextDidBeginEditingNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(scrollToActiveTextField) name:UITextFieldTextDidBeginEditingNotification object:nil];
