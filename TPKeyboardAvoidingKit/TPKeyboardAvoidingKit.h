daab7f3626774dc272ce2336b15dc5cb1662596a
Jerson Michael Perpetua <jersonperpetua@gmail.com>
2015-12-02 08:23:44 +0800

    Support Carthage by adding a shared framework target to the existing project file

---
+++ TPKeyboardAvoidingKit/TPKeyboardAvoidingKit.h
@@ -0,0 +1,22 @@
+//
+//  TPKeyboardAvoidingKit.h
+//  TPKeyboardAvoidingKit
+//
+//  Created by Jerson Michael Perpetua on 12/2/15.
+//  Copyright © 2015 A Tasty Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPKeyboardAvoidingKit.
+FOUNDATION_EXPORT double TPKeyboardAvoidingKitVersionNumber;
+
+//! Project version string for TPKeyboardAvoidingKit.
+FOUNDATION_EXPORT const unsigned char TPKeyboardAvoidingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPKeyboardAvoidingKit/PublicHeader.h>
+
+#import <TPKeyboardAvoidingKit/TPKeyboardAvoidingCollectionView.h>
+#import <TPKeyboardAvoidingKit/TPKeyboardAvoidingScrollView.h>
+#import <TPKeyboardAvoidingKit/TPKeyboardAvoidingTableView.h>
+#import <TPKeyboardAvoidingKit/UIScrollView+TPKeyboardAvoidingAdditions.h>
